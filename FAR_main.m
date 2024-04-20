eaffe81c3287aedc3f61fcb978d73cf6c5454158
Samantha <Troyandme04@gmail.com>
2024-04-20 17:22:49 -0400

    far with csv loading

--- FAR_main.m
+++ FAR_main.m
@@ -8,17 +8,10 @@ grid_size = 2; %UNITS????
 
 csv_file = "environment_files/GridLayout2.csv";
 
-% temp positions for testing, can be random later if we want
-start_pos = [1,1]*grid_size;% - (grid_size)/2; (To center within grid Point)
-goal_pos = [25,2]*grid_size;% - (grid_size)/2;
 
+robot_positions_csv = "Experiment_CSV/exp1_both.csv";
 
-start_pos2 = [10,2]*grid_size;% - (grid_size)/2; (To center within grid Point)
-goal_pos2 = [1,25]*grid_size;% - (grid_size)/2;
-
-start_pos3 = [35,20]*grid_size;% - (grid_size)/2; (To center within grid Point)
-goal_pos3 = [2,12]*grid_size;% - (grid_size)/2;
-
+robot_positions = get_position_and_goals(robot_positions_csv);
 
 %% Set up grid
 map = get_map_array(csv_file);
@@ -27,48 +20,50 @@ map = get_map_array(csv_file);
 %% Path planning
 fig_num = 1;
 G = FlowAnnotatedGraph(map, grid_size);
-[start,target] = getStartAndTarget(map,grid_size,start_pos,goal_pos);
-node_path = AstarDigraph(G,start,target,map,grid_size,start_pos,goal_pos,fig_num);
+for num_robots=1: size(robot_positions,1)
+    robot.start = robot_positions(num_robots, 1:2);
+    robot.goal = robot_positions(num_robots, 3:4);
+    robot.road = G
+    robot.original_road = robot.road;
+    robot.thetaInit = 45;
 
-[start2,target2] = getStartAndTarget(map,grid_size,start_pos2,goal_pos2);
-node_path2 = AstarDigraph(G,start2,target2);
+    robots(num_robots) = robot;
+end
 
-[start3,target3] = getStartAndTarget(map,grid_size,start_pos3,goal_pos3);
-node_path3 = AstarDigraph(G,start3,target3);
+if size(robots) >= 1
+    [start,target] = getStartAndTarget(map,grid_size,robots(1).start,robots(1).goal);
+    node_path = AstarDigraph(G,start,target,map,grid_size,robots(1).start,robots(1).goal,fig_num);
+    %Add Road With points to each bot
+    x_coo = G.Nodes.X(node_path);
+    y_coo = G.Nodes.Y(node_path);
+    road1 = [robots(1).start; x_coo,y_coo; robots(1).goal];
+    robots(1).node_path = node_path;
+end
 
-%Add Road With points to each bot
-x_coo = G.Nodes.X(node_path);
-y_coo = G.Nodes.Y(node_path);
-road1 = [start_pos; x_coo,y_coo; goal_pos];
-%Add Road With points to each bot
-x_coo = G.Nodes.X(node_path2);
-y_coo = G.Nodes.Y(node_path2);
-road2 = [start_pos2; x_coo,y_coo; goal_pos2];
-%Add Road With points to each bot
-x_coo = G.Nodes.X(node_path3);
-y_coo = G.Nodes.Y(node_path3);
-road3 = [start_pos3; x_coo,y_coo; goal_pos3];
+if size(robots) >= 2
+    [start2,target2] = getStartAndTarget(map,grid_size,robots(2).start,robots(2).goal);
+    node_path2 = AstarDigraph(G,start2,target2);
+    %Add Road With points to each bot
+    x_coo = G.Nodes.X(node_path2);
+    y_coo = G.Nodes.Y(node_path2);
+    road2 = [start_pos2; x_coo,y_coo; robots(2).goal];
+    robots(2).node_path = node_path2;
+end
 
-bot1.node_path = node_path;
-bot2.node_path = node_path2;
-bot3.node_path = node_path3;
+if size(robots) >= 3
+    [start3,target3] = getStartAndTarget(map,grid_size,robots(3).start,robots(3).goal);
+    node_path3 = AstarDigraph(G,start3,target3);
+    %Add Road With points to each bot
+    x_coo = G.Nodes.X(node_path3);
+    y_coo = G.Nodes.Y(node_path3);
+    road3 = [start_pos3; x_coo,y_coo; robots(3).goal];
+    robots(3).node_path = node_path3;
+end
 
-bot1.start = start_pos;
-bot2.start = start_pos2;
-bot3.start = start_pos3;
 
-bot1.goal = goal_pos;
-bot2.goal = goal_pos2;
-bot3.goal = goal_pos3;
-
-bot1.road = road1;
-bot2.road = road2;
-bot3.road = road3;
-
-robot_array = [bot1, bot2, bot3];
 k = 40; %reserve upto 5 steps (for now)
 timeStep = 0.1;
-[simulated_robots] = FARMultiRobotController(G,robot_array,k,timeStep);
+[simulated_robots] = FARMultiRobotController(G,robots,k,timeStep);
 
 figureNumber = 2;
 %[done] = vizualizeFARmotion3(simulated_robots,timeStep,map,grid_size,figureNumber);
